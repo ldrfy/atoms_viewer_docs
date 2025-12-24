873ebfa59e34c8f844f483e10d577e3679743758
yuhldr <yuhldr@qq.com>
2025-12-25 03:26:53 +0800

    feat: sample

---
+++ data/out.py
@@ -0,0 +1,29 @@
+import json
+import os
+
+
+def get_all_files_json(path_dir, output_json='files.json'):
+    """
+    遍历目录 directory 下的所有文件，保存文件名和路径到 JSON
+    """
+    file_list = []
+
+    for root, _dirs, files in os.walk(path_dir):
+        for f in files:
+            file_info = {
+                'fileName': f,
+                "label": f,
+                'url': f"https://raw.githubusercontent.com/ldrfy/atoms_viewer_docs/refs/heads/main/data/{os.path.join(root, f)}"
+            }
+            file_list.append(file_info)
+
+    # 保存为 JSON
+    with open(output_json, 'w', encoding='utf-8') as f:
+        json.dump(file_list, f, ensure_ascii=False, indent=4)
+
+    print(f"共找到 {len(file_list)} 个文件，已保存到 {output_json}")
+    return file_list
+
+
+# 示例
+get_all_files_json('samples', 'data.json')
